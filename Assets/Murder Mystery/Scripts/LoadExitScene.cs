506ca8408c8e2370f325942bfb8e726505e932ac
emiliehjoellund <mail@emiliehjoellund.dk>
2024-05-26 21:44:12 +0200

    refactor: Rename SceneChangerAfterAudio to LoadExitScene

--- Assets/Murder Mystery/Scripts/LoadExitScene.cs
+++ Assets/Murder Mystery/Scripts/LoadExitScene.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChangerAfterAudio : MonoBehaviour
+public class LoadExitScene : MonoBehaviour
 {
     public AudioSource audioSource;
     public string nextSceneName;
